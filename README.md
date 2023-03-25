fe3df833374db9189a257944d13970cf71d9bdd0
Elabar <leehoemun@gmail.com>
2023-03-25 13:37:54 +0800

    first commit

--- README.md
+++ README.md
@@ -1,38 +1,20 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Running the project
 
-## Getting Started
-
-First, run the development server:
-
-```bash
+```
+npm install
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+# Converting to data persistence with backend
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+The entire logic is written in the reducer function.
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+Actions that can be converted to **REST API**
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+- ADD_BOT
+- ADD_ORDER
+- WITHDRAW_BOT
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+In reality, the cooking bot should have a **callback** when the cooking is finished properly, so we do not need to convert `ADD_COOK_TIME`. The callback should trigger the backend to try to assign order to the bot again.
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+If implementing in a proper backend, we might need to use **socket** to retrieve the database changes for bot and order to update the UI accordingly.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,123 +1,345 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import { useEffect, useReducer } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const BOT_COOK_TIME = 10;
+
+enum Actions {
+  ADD_BOT = "ADD_BOT",
+  ADD_ORDER = "ADD_ORDER",
+  WITHDRAW_BOT = "WITHDRAW_BOT",
+  ADD_COOK_TIME = "ADD_COOK_TIME",
+}
+
+interface POS {
+  bots: Bot[];
+  queue: Order[];
+  bot_count: number;
+  order_count: number;
+}
+
+type BotStatus = "idle" | "cooking";
+
+interface Bot {
+  id: string;
+  status: BotStatus;
+  handling_order: string | null;
+  remaining_seconds: number | null;
+}
+type OrderStatus = "pending" | "completed" | "cooking";
+type OrderType = "normal" | "vip";
+
+interface Order {
+  order_number: string;
+  status: OrderStatus;
+  type: OrderType;
+  remaining_seconds: null | number;
+}
+
+type CommonAction = {
+  type: typeof Actions.ADD_BOT | typeof Actions.ADD_COOK_TIME;
+};
+
+type AddOrderAction = {
+  type: typeof Actions.ADD_ORDER;
+  order_type: OrderType;
+};
+
+type WithdrawBotAction = {
+  type: typeof Actions.WITHDRAW_BOT;
+  bot_id: string;
+};
+
+const get_pending_order_and_idle_bot = (queue: Order[], bots: Bot[]) => {
+  let order_idx = -1;
+  let bot_idx = -1;
+  if (queue.length > 0) {
+    bot_idx = bots.findIndex((e) => e.status === "idle");
+    if (bot_idx !== -1) {
+      order_idx = queue.findIndex(
+        (e) => e.type === "vip" && e.status === "pending"
+      );
+      if (order_idx === -1) {
+        order_idx = queue.findIndex((e) => e.status === "pending");
+      }
+    }
+  }
+
+  return {
+    order_idx,
+    bot_idx,
+  };
+};
+
+// will mutate the params
+const assign_order_to_bot = (state: POS) => {
+  const { order_idx, bot_idx } = get_pending_order_and_idle_bot(
+    state.queue,
+    state.bots
+  );
+  if (order_idx !== -1 && bot_idx !== -1) {
+    state.queue[order_idx].status = "cooking";
+    state.queue[order_idx].remaining_seconds = BOT_COOK_TIME;
+    state.bots[bot_idx].status = "cooking";
+    state.bots[bot_idx].remaining_seconds = BOT_COOK_TIME;
+    state.bots[bot_idx].handling_order = state.queue[order_idx].order_number;
+  }
+};
+
+const reducer = (
+  state: POS,
+  action: CommonAction | AddOrderAction | WithdrawBotAction
+): POS => {
+  // bad
+  const new_state: POS = JSON.parse(JSON.stringify(state));
+  switch (action.type) {
+    case Actions.ADD_BOT: {
+      const new_bot: Bot = {
+        handling_order: null,
+        id: `BT${(new_state.bot_count + 1).toString()}`,
+        status: "idle",
+        remaining_seconds: null,
+      };
+      new_state.bot_count++;
+      new_state.bots.push(new_bot);
+      assign_order_to_bot(new_state);
+      return new_state;
+    }
+    case Actions.ADD_ORDER: {
+      new_state.queue.push({
+        order_number: `ON${(new_state.order_count + 1).toString()}`,
+        status: "pending",
+        type: action.order_type,
+        remaining_seconds: null,
+      });
+      new_state.order_count++;
+      assign_order_to_bot(new_state);
+      return new_state;
+    }
+    case Actions.WITHDRAW_BOT: {
+      const bot_idx = new_state.bots.findIndex((e) => e.id === action.bot_id);
+      if (bot_idx !== -1) {
+        const withdrawn_bots = new_state.bots.splice(bot_idx, 1);
+        const bot = withdrawn_bots[0];
+        if (bot.status === "cooking") {
+          const order_idx = new_state.queue.findIndex(
+            (e) => e.order_number === bot.handling_order
+          );
+          if (order_idx !== -1) {
+            new_state.queue[order_idx].status = "pending";
+            new_state.queue[order_idx].remaining_seconds = null;
+          }
+        }
+      }
+      return new_state;
+    }
+    case Actions.ADD_COOK_TIME: {
+      let changed = 0;
+      for (let i = 0; i < new_state.bots.length; i++) {
+        const bot = new_state.bots[i];
+        if (bot.status === "cooking") {
+          changed++;
+          const order_idx = new_state.queue.findIndex(
+            (e) => e.order_number === bot.handling_order
+          );
+          let order: Order | undefined;
+          if (order_idx !== -1) {
+            order = new_state.queue[order_idx];
+          }
+          if (bot.remaining_seconds === 0) {
+            if (order) {
+              order.status = "completed";
+              order.remaining_seconds = null;
+            }
+            bot.remaining_seconds = null;
+            bot.status = "idle";
+            bot.handling_order = null;
+            assign_order_to_bot(new_state);
+          } else {
+            if (bot.remaining_seconds !== null) {
+              bot.remaining_seconds--;
+            }
+            if (order && order.remaining_seconds !== null) {
+              order.remaining_seconds--;
+            }
+          }
+        }
+      }
+      if (!changed) {
+        // return original state to avoid render
+        return state;
+      }
+      return new_state;
+    }
+    default:
+      return new_state;
+  }
+};
+
+const initialState: POS = {
+  bots: [],
+  queue: [],
+  bot_count: 0,
+  order_count: 0,
+};
+
+const OrderBox = ({ order }: { order: Order }) => {
+  return (
+    <div
+      key={order.order_number}
+      style={{
+        textAlign: "center",
+        border: "1px solid red",
+        padding: 6,
+      }}
+    >
+      <h3>{`${order.order_number} [${order.type}]`}</h3>
+      <p>{order.status}</p>
+      {order.status === "cooking" && (
+        <p>{`Time: ${order.remaining_seconds}s`}</p>
+      )}
+    </div>
+  );
+};
+
+const OrderSection = ({ label, queue }: { label: string; queue: Order[] }) => {
+  return (
+    <fieldset
+      style={{
+        display: "flex",
+        gap: 10,
+        flexWrap: "wrap",
+        margin: 10,
+        padding: 10,
+      }}
+    >
+      <legend>{label}</legend>
+      {queue.map((order) => {
+        return <OrderBox order={order} key={order.order_number} />;
+      })}
+    </fieldset>
+  );
+};
+
+const usePos = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      dispatch({ type: Actions.ADD_COOK_TIME });
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const addBot = () => {
+    dispatch({ type: Actions.ADD_BOT });
+  };
+
+  const addOrder = (order_type: OrderType) => {
+    dispatch({ type: Actions.ADD_ORDER, order_type });
+  };
+  const withdrawBot = (bot_id: string) => {
+    dispatch({ type: Actions.WITHDRAW_BOT, bot_id });
+  };
+
+  const pendingQueue = [
+    ...state.queue.filter((e) => e.type === "vip" && e.status === "pending"),
+    ...state.queue.filter((e) => e.type === "normal" && e.status === "pending"),
+  ];
+  const cookingQueue = state.queue.filter((e) => e.status === "cooking");
+
+  const completedQueue = state.queue.filter((e) => e.status === "completed");
+
+  return {
+    bots: state.bots,
+    completedQueue,
+    cookingQueue,
+    pendingQueue,
+    addBot,
+    addOrder,
+    withdrawBot,
+    queue: state.queue,
+  };
+};
 
 export default function Home() {
+  const {
+    bots,
+    pendingQueue,
+    addBot,
+    addOrder,
+    withdrawBot,
+    completedQueue,
+    cookingQueue,
+  } = usePos();
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>McD Bot POS</title>
         <meta name="description" content="Generated by create next app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+      <div>
+        <div>
+          <div style={{ display: "flex", gap: 10 }}>
+            <h1>Bots</h1>
+            <button onClick={addBot}>Add bot</button>
+          </div>
+          <div style={{ display: "flex", gap: 10, flexWrap: "wrap" }}>
+            {bots.map((bot) => {
+              return (
+                <div
+                  key={bot.id}
+                  style={{
+                    textAlign: "center",
+                    border: "1px solid red",
+                    padding: 6,
+                  }}
+                >
+                  <button
+                    onClick={() => {
+                      withdrawBot(bot.id);
+                    }}
+                  >
+                    Remove bot
+                  </button>
+                  <h3>{bot.id}</h3>
+                  <p>
+                    {bot.status}{" "}
+                    {bot.status === "cooking" && `[${bot.handling_order}]`}
+                  </p>
+                  {bot.status === "cooking" && (
+                    <p>{`Time: ${bot.remaining_seconds}s`}</p>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <br />
+        <div>
+          <div style={{ display: "flex", gap: 10 }}>
+            <h1>Orders</h1>
+            <button
+              onClick={() => {
+                addOrder("normal");
+              }}
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+              Add Normal Order
+            </button>
+            <button
+              onClick={() => {
+                addOrder("vip");
+              }}
+            >
+              Add VIP Order
+            </button>
           </div>
+          <OrderSection label="Completed" queue={completedQueue} />
+          <OrderSection label="Cooking" queue={cookingQueue} />
+          <OrderSection label="Pending" queue={pendingQueue} />
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      </div>
     </>
-  )
+  );
 }
